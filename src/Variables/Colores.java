5a29d114369f0ab5a453cad0ea13d7d855936000
Ruben Hernandez <hernandzruben9@gmail.com>
2023-10-17 18:03:43 -0400

    lo que hicimos hoy

--- src/Variables/Colores.java
+++ src/Variables/Colores.java
@@ -12,7 +12,7 @@ public final class Colores {
 	public static final Color TEXTO_OSCURO = new Color(0x252525);
 	public static final Color TEXTO_GRIS = new Color(0x989898);
 	public static final Color ROJO_ERROR = new Color(0xA5473D);
-	public static final Color BLANCO = new Color(0xFFFFFF);
+	public static final Color BLANCO = new Color(0xFDFDFD);
 	public static final Color NEGRO = new Color(0x000000);
 	public static final Color ROJO = new Color(0xAE070F);
 	

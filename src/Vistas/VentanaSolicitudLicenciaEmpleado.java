7f32da1636bea91489d70ee7b79186178517ffc1
Marcel <marcel020568@gmail.com>
2023-10-18 01:19:40 -0400

    Nuevos cambios

--- src/Vistas/VentanaSolicitudLicenciaEmpleado.java
+++ src/Vistas/VentanaSolicitudLicenciaEmpleado.java
@@ -17,33 +17,43 @@ import javax.swing.JComboBox;
 import javax.swing.JSpinner;
 
 import java.awt.Font;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.swing.border.TitledBorder;
 import javax.swing.JButton;
 import javax.swing.SpinnerNumberModel;
 
-public class VentanaSolicitudBajaEmpleado extends JFrame {
+import com.toedter.calendar.JDateChooser;
 
+public class VentanaSolicitudLicenciaEmpleado extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JPanel panel_datos;
-	private JTextField textField_NombreEmpleado;
-	private JTextField textField_PrimerApellido;
-	private JTextField textField_SegundoApellido;
-	private JTextField textField_Carnet;
 	private JPanel panelBase;
+	private JPanel panel_datos;
+	private JLabel nombre_empleado;
+	private JTextField textField_NombreEmpleado;
+	private JLabel labelPrimer_apellido;
+	private JTextField textField_PrimerApellido;
+	private JLabel labelSegundoApellido;
+	private JTextField textField_SegundoApellido;
+	private JLabel labelCarnetID;
+	private JTextField textField_Carnet;
+	private JComboBox comboBox_Facultades;
+	private JSpinner spinner_NoTrabajador;
+	private JComboBox comboBox_Profesiones;
 	private JButton btnEnviarSolicitud;
 	private JButton btnCancelar;
-	private JComboBox comboBox_Profesiones;
-	private JSpinner spinner_NoTrabajador;
-	private JComboBox comboBox_Facultades;
-	private JLabel labelCarnetID;
-	private JLabel labelSegundoApellido;
-	private JLabel labelPrimer_apellido;
-	private JLabel nombre_empleado;
-	private JPanel panel_foto;
-	private JLabel labelFoto;
 	private JLabel labelMotivo;
 	private JTextField textField_Motivo;
+	private JPanel panel_foto;
+	private JLabel labelFoto;
+	private JDateChooser fechaSalida;
+	private JDateChooser fechaRegreso;
 
 	/**
 	 * Launch the application.
@@ -52,7 +62,7 @@ public class VentanaSolicitudBajaEmpleado extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					VentanaSolicitudBajaEmpleado frame = new VentanaSolicitudBajaEmpleado();
+					VentanaSolicitudLicenciaEmpleado frame = new VentanaSolicitudLicenciaEmpleado();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -64,22 +74,22 @@ public class VentanaSolicitudBajaEmpleado extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public VentanaSolicitudBajaEmpleado() {
-		setTitle("Llenar solicitud de baja");
+	public VentanaSolicitudLicenciaEmpleado() {
+		setTitle("Llenar solicitud de licencia");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 672, 457);
+		setBounds(100, 100, 684, 457);
 		contentPane = new JPanel();
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		panelBase = new JPanel();
 		panelBase.setBackground(Color.WHITE);
-		panelBase.setBounds(0, 0, 656, 418);
+		panelBase.setBounds(0, 0, 684, 418);
 		contentPane.add(panelBase);
 		panelBase.setLayout(null);
 		
 		panel_datos = new JPanel();
-		panel_datos.setBounds(299, 11, 347, 384);
+		panel_datos.setBounds(299, 0, 357, 407);
 		panelBase.add(panel_datos);
 		panel_datos.setBackground(new Color (204, 255, 204));
 		panel_datos.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0), 2), "Datos del empleado", TitledBorder.LEADING, TitledBorder.TOP, new Font("Roboto Medium", Font.PLAIN, 18), new Color(0, 0, 0)));
@@ -98,74 +108,72 @@ public class VentanaSolicitudBajaEmpleado extends JFrame {
 		
 		labelPrimer_apellido = new JLabel("Primer Apellido:");
 		labelPrimer_apellido.setFont(new Font("Tahoma", Font.BOLD, 14));
-		labelPrimer_apellido.setBounds(21, 66, 116, 19);
+		labelPrimer_apellido.setBounds(21, 60, 116, 19);
 		panel_datos.add(labelPrimer_apellido);
 		
 		textField_PrimerApellido = new JTextField();
-		textField_PrimerApellido.setBounds(142, 65, 161, 20);
+		textField_PrimerApellido.setBounds(142, 61, 161, 20);
 		textField_PrimerApellido.setBorder(new LineBorder(new Color(0,0,0), 1));
 		panel_datos.add(textField_PrimerApellido);
-		textField_PrimerApellido.setColumns(10);
 		
 		labelSegundoApellido = new JLabel("Segundo Apellido:");
 		labelSegundoApellido.setFont(new Font("Tahoma", Font.BOLD, 14));
-		labelSegundoApellido.setBounds(21, 102, 127, 19);
+		labelSegundoApellido.setBounds(21, 98, 127, 19);
 		panel_datos.add(labelSegundoApellido);
 		
 		textField_SegundoApellido = new JTextField();
-		textField_SegundoApellido.setBounds(152, 101, 151, 20);
+		textField_SegundoApellido.setBounds(152, 97, 151, 20);
 		textField_SegundoApellido.setBorder(new LineBorder(new Color(0,0,0), 1));
 		panel_datos.add(textField_SegundoApellido);
 		textField_SegundoApellido.setColumns(10);
 		
 		JLabel labelFacultad = new JLabel("Facultad:");
 		labelFacultad.setFont(new Font("Tahoma", Font.BOLD, 14));
-		labelFacultad.setBounds(21, 169, 71, 14);
+		labelFacultad.setBounds(21, 165, 71, 14);
 		panel_datos.add(labelFacultad);
 		
 		labelCarnetID = new JLabel("Carnet de Identidad:");
 		labelCarnetID.setFont(new Font("Tahoma", Font.BOLD, 14));
-		labelCarnetID.setBounds(21, 138, 145, 14);
+		labelCarnetID.setBounds(21, 134, 145, 14);
 		panel_datos.add(labelCarnetID);
 		
 		textField_Carnet = new JTextField();
-		textField_Carnet.setBounds(176, 137, 127, 20);
-		textField_Carnet.setBorder(new LineBorder(new Color(0,0,0), 1));
+		textField_Carnet.setBounds(176, 133, 127, 20);
+	    textField_Carnet.setBorder(new LineBorder(new Color(0,0,0), 1));
 		panel_datos.add(textField_Carnet);
-		textField_Carnet.setColumns(10);
 		
 		comboBox_Facultades = new JComboBox();
 		comboBox_Facultades.setBackground(Color.WHITE);
 		comboBox_Facultades.setModel(new DefaultComboBoxModel(new String[] {"<Facultad>","Informática", "Automática", "Mecánica", "Telecomunicaciones","Química","Civil","Arquitectura","Industrial","Eléctrica"}));
-		comboBox_Facultades.setBounds(101, 168, 202, 20);
+		comboBox_Facultades.setBounds(101, 164, 202, 20);
 		panel_datos.add(comboBox_Facultades);
 		
 		spinner_NoTrabajador = new JSpinner();
 		spinner_NoTrabajador.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		spinner_NoTrabajador.setBorder(new LineBorder(new Color(0,0,0), 1));
 		spinner_NoTrabajador.setModel(new SpinnerNumberModel(1, 1, 100000, 1));
-		spinner_NoTrabajador.setBounds(142, 198, 161, 20);
+		spinner_NoTrabajador.setBounds(142, 194, 161, 20);
 		panel_datos.add(spinner_NoTrabajador);
 		
 		JLabel labelNoTrabajador = new JLabel("No. Trabajador:");
 		labelNoTrabajador.setFont(new Font("Tahoma", Font.BOLD, 14));
-		labelNoTrabajador.setBounds(21, 200, 117, 17);
+		labelNoTrabajador.setBounds(21, 196, 117, 17);
 		panel_datos.add(labelNoTrabajador);
 		
 		JLabel labelProfesion = new JLabel("Profesi\u00F3n u oficio:");
 		labelProfesion.setFont(new Font("Tahoma", Font.BOLD, 14));
-		labelProfesion.setBounds(21, 233, 142, 14);
+		labelProfesion.setBounds(21, 229, 142, 14);
 		panel_datos.add(labelProfesion);
 		
 		comboBox_Profesiones = new JComboBox();
 		comboBox_Profesiones.setBackground(Color.WHITE);
 		comboBox_Profesiones.setModel(new DefaultComboBoxModel(new String[] {"<Profesiones>","Docente", "Investigador", "Técnico", "Dirigente","Obrero","Servicio","Administrador"}));
-		comboBox_Profesiones.setBounds(152, 232, 151, 20);
+		comboBox_Profesiones.setBounds(152, 228, 151, 20);
 		panel_datos.add(comboBox_Profesiones);
 		
 		btnEnviarSolicitud = new JButton("Enviar solicitud");
 		btnEnviarSolicitud.setBackground(Color.WHITE);
-		btnEnviarSolicitud.setBounds(21, 310, 127, 25);
+		btnEnviarSolicitud.setBounds(21, 365, 127, 25);
 		btnEnviarSolicitud.setBorder(new LineBorder(new Color(0,0,0), 1));
 		panel_datos.add(btnEnviarSolicitud);
 		btnEnviarSolicitud.setFont(new Font("Tahoma", Font.BOLD, 14));
@@ -173,21 +181,30 @@ public class VentanaSolicitudBajaEmpleado extends JFrame {
 		btnCancelar = new JButton("Cancelar");
 		btnCancelar.setBackground(Color.WHITE);
 		btnCancelar.setBorder(new LineBorder(new Color(0,0,0), 1));
-		btnCancelar.setBounds(174, 310, 129, 25);
+		btnCancelar.setBounds(174, 365, 129, 25);
 		panel_datos.add(btnCancelar);
 		btnCancelar.setFont(new Font("Tahoma", Font.BOLD, 14));
 		
 		labelMotivo = new JLabel("Motivo:");
 		labelMotivo.setFont(new Font("Tahoma", Font.BOLD, 14));
 		labelMotivo.setIgnoreRepaint(true);
-		labelMotivo.setBounds(21, 267, 86, 14);
+		labelMotivo.setBounds(21, 263, 86, 14);
 		panel_datos.add(labelMotivo);
 		
 		textField_Motivo = new JTextField();
-		textField_Motivo.setBounds(84, 265, 219, 20);
+		textField_Motivo.setBounds(84, 261, 219, 20);
 		textField_Motivo.setBorder(new LineBorder(new Color(0,0,0), 1));
 		panel_datos.add(textField_Motivo);
-		textField_Motivo.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Fecha Salida:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(21, 297, 99, 14);
+		panel_datos.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Fecha Regreso:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_1.setBounds(21, 331, 116, 17);
+		panel_datos.add(lblNewLabel_1);
 		
 		panel_foto = new JPanel();
 		panel_foto.setBounds(0, 0, 299, 418);
@@ -200,5 +217,19 @@ public class VentanaSolicitudBajaEmpleado extends JFrame {
 		labelFoto.setBounds(0, 0, 299, 418);
 		labelFoto.setIcon(ico);
 		panel_foto.add(labelFoto);
+		
+		fechaSalida = new JDateChooser();
+		fechaSalida.setDateFormatString("dd/MM/yyyy");
+		fechaSalida.setMinSelectableDate(new Date());
+		fechaSalida.setBounds(127, 294, 176, 20);
+		panel_datos.add(fechaSalida);
+		
+		fechaRegreso = new JDateChooser();
+		fechaRegreso.setDateFormatString("dd/MM/yyyy");
+		fechaRegreso.setBounds(137, 331, 166, 20);
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.DAY_OF_YEAR,1);
+		fechaRegreso.setMinSelectableDate(calendar.getTime());
+		panel_datos.add(fechaRegreso);
 	}
 }

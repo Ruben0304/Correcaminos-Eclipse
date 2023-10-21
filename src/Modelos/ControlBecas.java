8b9e359ee2ccfb7ebd080f3682399da2f72c6b36
Ruben Hernandez <hernandzruben9@gmail.com>
2023-10-21 15:03:08 -0400

    hy

--- src/Modelos/ControlBecas.java
+++ src/Modelos/ControlBecas.java
@@ -6,6 +6,8 @@ public class ControlBecas {
     private ArrayList<String> idEstudiantesEntregaronCarnet;
     private ArrayList<String> ididEstudiantesEntregaronPertenencias;
 
+
+    //verifica si el usuario entrego carnet de becado a partir de recorrer la lista de los estudiantes que lo entregaron
     public boolean entregoCarnet(String id) {
         boolean encontrado = false;
         for (int i = 0; i < idEstudiantesEntregaronCarnet.size() && !encontrado; i++) {
@@ -14,6 +16,7 @@ public class ControlBecas {
         return encontrado;
     }
 
+    //verifica si el usuario entrego las pertenencias de la beca a partir de recorrer la lista de los estudiantes que las entregaron
     public boolean entregoPertenencias(String id) {
         boolean encontrado = false;
         for (int i = 0; i < ididEstudiantesEntregaronPertenencias.size() && !encontrado; i++) {

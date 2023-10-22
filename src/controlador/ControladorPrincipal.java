76879b8ae4c53fa128f5966488f825ca58a2b9d5
Ruben Hernandez <hernandzruben9@gmail.com>
2023-10-22 04:25:59 -0400

    ggg

--- src/controlador/ControladorPrincipal.java
+++ src/controlador/ControladorPrincipal.java
@@ -1,19 +1,22 @@
 package controlador;
 
 import modelos.entidades.*;
-
+import vistas.Inicio;
 
 public class ControladorPrincipal {
-    
-    public static void mostrarInicio(Usuario usuario) {
-        
-        if (usuario instanceof Estudiante) {
-           if (usuario instanceof Becado) {
-            
-           } else {
-            
-           }
 
-        } 
+    public static void mostrarInicio(Usuario usuario) {
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+        // inicio.dispose();
+        // if (usuario instanceof Estudiante) {
+        // if (usuario instanceof Becado) {
+
+        // } else {
+        // inicio.setVisible(true);
+        // inicio.dispose();
+        // }
+
+        // }
     }
 }

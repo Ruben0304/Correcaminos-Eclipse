2cd7fba90bf328ac5506609b56c105f933e97ecb
Ruben Hernandez <hernandzruben9@gmail.com>
2023-10-21 21:56:21 -0400

    dd

--- src/Modelos/SeguridadInformatica.java
+++ src/Modelos/SeguridadInformatica.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class SeguridadInformatica {
     private ArrayList<String> idEstudiantesCancelaronCuentas;
 
-public boolean canceloCuenta(String id) {
+public boolean comprobarCierreDeCuenta(String id) {
         boolean encontrado = false;
         for (int i = 0; i < idEstudiantesCancelaronCuentas.size() && !encontrado; i++) {
             encontrado = idEstudiantesCancelaronCuentas.get(i).equals(id);

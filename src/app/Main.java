fc4c576d499d60e118333cb7178fa61d4a856d00
Ruben Hernandez <hernandzruben9@gmail.com>
2023-10-22 12:32:21 -0400

    dd

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,8 @@
 package app;
 import java.util.ArrayList;
 
-
+import modelos.datos.AdminFactory;
+import modelos.entidades.Admin;
 import modelos.entidades.Secretaria;
 import modelos.entidades.Usuario;
 import vistas.auth.Login;
@@ -10,8 +11,14 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Secretaria secretaria = new Secretaria();
+
+		ArrayList<Admin> admins = new ArrayList<>();
+		admins.addAll(AdminFactory.inicializarAdmins());
+
 		ArrayList<Usuario> usuarios = new ArrayList<>();
 		usuarios.addAll(secretaria.getEstudiantes());
+		usuarios.addAll(admins);
+		
 		Login loginView = new Login(usuarios);
 	    loginView.setVisible(true);
 

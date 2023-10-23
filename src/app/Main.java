2d8016c9b6ff6f7a755bf2d49a6cd14d1def1921
Ruben Hernandez <hernandzruben9@gmail.com>
2023-10-23 00:11:24 -0400

    s

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,22 +7,23 @@ import modelos.auth.GestorUsuarios;
 
 import modelos.entidades.RecursosHumanos;
 import modelos.entidades.Secretaria;
-
-
+import vistas.auth.Login;
 
 public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Secretaria secretaria = new Secretaria();
-                    RecursosHumanos recursosHumanos = new RecursosHumanos();
-                    GestorUsuarios gestorUsuarios = new GestorUsuarios(secretaria, recursosHumanos, true);
-                    ControladorLogin.mostrarLogin(gestorUsuarios.getUsuarios(), secretaria, recursosHumanos);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Secretaria secretaria = new Secretaria();
+					RecursosHumanos recursosHumanos = new RecursosHumanos();
+					GestorUsuarios gestorUsuarios = new GestorUsuarios(secretaria, recursosHumanos, true);
+					Login login = new Login(gestorUsuarios.getUsuarios(), secretaria, recursosHumanos);
+					login.setVisible(true);
+					// ControladorLogin.mostrarLogin(gestorUsuarios.getUsuarios(), secretaria, recursosHumanos);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }

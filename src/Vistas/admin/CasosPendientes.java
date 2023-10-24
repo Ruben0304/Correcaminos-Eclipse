38d165087e4e3a5c3bd3425d338e3a0b809bd9a0
Ruben Hernandez <hernandzruben9@gmail.com>
2023-10-24 02:02:51 -0400

    fgfg

--- src/Vistas/admin/CasosPendientes.java
+++ src/Vistas/admin/CasosPendientes.java
@@ -6,6 +6,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 
+import controlador.ControladorAdmin;
+import modelos.auth.GestorUsuarios;
 import modelos.entidades.Estudiante;
 import modelos.entidades.Persona;
 import modelos.entidades.RecursosHumanos;
@@ -20,84 +22,74 @@ import java.awt.event.ActionListener;
 
 public class CasosPendientes extends JFrame {
 
-	private DefaultListModel<String> listModel;
-	private JTable table;
-	private JTable table_2;
-    
-	/**
-	 * Create the frame.
-	 */
-	public CasosPendientes(final Usuario usuarioAutenticado, final ArrayList<Usuario> usuarios,
-			final Secretaria secretaria, final RecursosHumanos recursosHumanos,
-			final ArrayList<Persona> usuariosPendientes) {
+    private DefaultListModel<String> listModel;
+    private JTable table;
+    private JTable table_2;
 
-		setResizable(false);
-		setTitle("Casos Pendientes");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(786, 645);
-		setLocationRelativeTo(null);
-		getContentPane().setLayout(null);
-        
-                        
-		
+    /**
+     * Create the frame.
+     */
+    public CasosPendientes(final Usuario usuarioAutenticado, final ArrayList<Usuario> usuarios,
+            final Secretaria secretaria, final RecursosHumanos recursosHumanos,
+            final ArrayList<Persona> usuariosPendientes) {
 
-        
-        
-        
+        setResizable(false);
+        setTitle("Casos Pendientes");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(786, 645);
+        setLocationRelativeTo(null);
+        getContentPane().setLayout(null);
 
-//        JList<String> userList = new JList<>(new AbstractListModel() {
-//        	String[] values = new String[] {"ddd", "trt", "u"};
-//        	public int getSize() {
-//        		return values.length;
-//        	}
-//        	public Object getElementAt(int index) {
-//        		return values[index];
-//        	}
-//        });
-//        userList.setBounds(60, 170, 500, 72);
-//        getContentPane().add(userList);
+        // JList<String> userList = new JList<>(new AbstractListModel() {
+        // String[] values = new String[] {"ddd", "trt", "u"};
+        // public int getSize() {
+        // return values.length;
+        // }
+        // public Object getElementAt(int index) {
+        // return values[index];
+        // }
+        // });
+        // userList.setBounds(60, 170, 500, 72);
+        // getContentPane().add(userList);
 
+        JButton confirmButton = new JButton("Confirmar Entrega");
+        confirmButton.setBounds(589, 517, 137, 25);
+        getContentPane().add(confirmButton);
         
-        
-               
-                JButton confirmButton = new JButton("Confirmar Entrega");
-                confirmButton.setBounds(589, 517, 137, 25);
-                getContentPane().add(confirmButton);
-                
-                Object[][] data = new Object[usuariosPendientes.size()][3];
-                int i = 0;
-                for(Persona p : usuariosPendientes){
-                    data[i][0] = p.getCi();
-                    data[i][1] = p.getNombre();
-                    data[i][2] = p.getApellidos();
-                    i++;
-                }
-                
-                table_2 = new JTable();
-                table_2.setModel(new DefaultTableModel(
-                    data,
-                    new String[] {
+        Object[][] data = new Object[usuariosPendientes.size()][3];
+        int i = 0;
+        for (Persona p : usuariosPendientes) {
+            data[i][0] = p.getCi();
+            data[i][1] = p.getNombre();
+            data[i][2] = p.getApellidos();
+            i++;
+        }
+
+        table_2 = new JTable();
+        table_2.setModel(new DefaultTableModel(
+                data,
+                new String[] {
                         "Carnet", "Nombre", "Apellidos"
-                    }
-                ));
-                table_2.setBounds(112, 262, 330, 247);
-                getContentPane().add(table_2);
-                
-               
-                
-                
-                confirmButton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                       
-//                        int selectedIndex = userList.getSelectedIndex();
-                       
-                    }
-                });
-    
-		
+                }));
+        table_2.setBounds(112, 33, 587, 476);
+        getContentPane().add(table_2);
 
-	
+        confirmButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-	}
+                int selectedRow = table_2.getSelectedRow();
+                if (selectedRow != -1) {
+                    String carnet = table_2.getValueAt(selectedRow, 0).toString();
+                    
+                  ((Estudiante)GestorUsuarios.buscarUsuarioPorCi(carnet, usuariosPendientes)).setLibrosDeBiblioteca(false);
+                   ControladorAdmin.mostrarGestionLicencias(usuarioAutenticado, usuarios, secretaria, recursosHumanos);
+                } else {
+                    // No se ha seleccionado ninguna fila
+                }
+
+            }
+        });
+
+    }
 }

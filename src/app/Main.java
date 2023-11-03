e087ab6e818e0e2fc59e2c83bc939f37c198b0fc
Ruben Hernandez <hernandzruben9@gmail.com>
2023-11-03 18:23:35 -0400

    c

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,13 +6,14 @@ import controlador.ControladorLogin;
 import controlador.ControladorPrincipal;
 import modelos.gestion.GestorPrincipal;
 
+
 public class Main {
 	public static void main(String[] args) {
 		GestorPrincipal.getGestorPrincipal();
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					
+
 					ControladorLogin.comprobarSession();
 					ControladorPrincipal.mostrarInicio();
 

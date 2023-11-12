cccb7bb1a1583553857241d87c26646fc1c07fe2
Ruben Hernandez <hernandzruben9@gmail.com>
2023-11-12 12:03:48 -0500

    hh

--- src/models/gestion/estudiantes/GestorEstudiantes.java
+++ src/models/gestion/estudiantes/GestorEstudiantes.java
@@ -12,7 +12,7 @@ import models.interfaces.Actualizador;
 import models.responsabilidades.ResponsabilidadesEstudiantes;
 import models.usuarios.Estudiante;
 
-public class GestorEstudiantes implements Actualizador{
+public class GestorEstudiantes implements Actualizador {
     private GestorSolicitudesEstudiante gestorSolicitudes;
     private GestorResponsabilidadesEstudiantes gestorResponsabilidadesEstudiantes;
     private ArrayList<Estudiante> estudiantes;
@@ -21,27 +21,26 @@ public class GestorEstudiantes implements Actualizador{
     public GestorEstudiantes() {
         estudiantes = new ArrayList<>();
         registrarEstudiantes();
-        gestorSolicitudes=new GestorSolicitudesEstudiante(estudiantes);
-
+        gestorSolicitudes = new GestorSolicitudesEstudiante(estudiantes);
+        resp = new ArrayList<>();
         for (Estudiante estudiante : estudiantes) {
 
             this.resp.add(new ResponsabilidadesEstudiantes(estudiante.getResponsabilidades(), estudiante));
         }
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String jsonAdmins = gson.toJson(resp);
-		try {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String jsonAdmins = gson.toJson(resp);
+        try {
 
-			FileWriter writerA = new FileWriter("./responsabilidadesEstudiantes.json");
-			writerA.write(jsonAdmins);
-			writerA.close();
+            FileWriter writerA = new FileWriter("./responsabilidadesEstudiantes.json");
+            writerA.write(jsonAdmins);
+            writerA.close();
 
-			System.out.println("Guardado todo ok.");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        
+            System.out.println("Guardado todo ok.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
-    
 
     private void registrarEstudiantes() {
         this.estudiantes = ObtenerEstudinetes.cargarDesdeArchivo();

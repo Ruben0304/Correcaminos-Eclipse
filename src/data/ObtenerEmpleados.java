cc64a1f263d6599b147779565d04578a2cb4874b
Ruben Hernandez <hernandzruben9@gmail.com>
2023-12-02 16:36:09 -0500

    d

--- src/data/ObtenerEmpleados.java
+++ src/data/ObtenerEmpleados.java
@@ -2,6 +2,7 @@ package data;
 
 import java.util.ArrayList;
 
+import models.usuarios.Docente;
 import models.usuarios.Empleado;
 import models.usuarios.NoDocente;
 
@@ -13,24 +14,36 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ObtenerEmpleados {
-	
-	public static ArrayList<Empleado> cargarDesdeArchivo(){
-		 
-	    Gson gson = new Gson();
-        
-	    ArrayList<Empleado> empleados = new ArrayList<>();
+
+    public static ArrayList<Empleado> cargarDesdeArchivo() {
+
+        Gson gson = new Gson();
+
+        ArrayList<Empleado> empleados = new ArrayList<>();
+        ArrayList<NoDocente> noDocentes = new ArrayList<>();
+        ArrayList<Docente> docentes = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader("./jsons/nodocentes.json"))) {
-            Type listType = new TypeToken<ArrayList<Empleado>>() {
+            Type listType = new TypeToken<ArrayList<NoDocente>>() {
             }.getType();
 
-            empleados = gson.fromJson(reader, listType);
+            noDocentes = gson.fromJson(reader, listType);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader("./jsons/docentes.json"))) {
+            Type listType = new TypeToken<ArrayList<Docente>>() {
+            }.getType();
+
+            noDocentes = gson.fromJson(reader, listType);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        empleados.addAll(noDocentes);
+        empleados.addAll(docentes);
         return empleados;
-	}
+    }
 }
-

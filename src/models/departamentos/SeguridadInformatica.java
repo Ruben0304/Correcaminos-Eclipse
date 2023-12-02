6841209ccfb522e581abc38be271a5dee521cc6d
mpfría <116919819+MarcelPulido@users.noreply.github.com>
2023-12-02 11:57:26 -0500

    departamentos update

--- src/models/departamentos/SeguridadInformatica.java
+++ src/models/departamentos/SeguridadInformatica.java
@@ -1,80 +1,65 @@
  package models.departamentos;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 import models.interfaces.VerificadorEmpleado;
 import models.interfaces.VerificadorEstudiante;
-import models.responsabilidades.ResponsabilidadesEmpleados;
-import models.responsabilidades.ResponsabilidadesEstudiantes;
 import models.usuarios.Empleado;
 import models.usuarios.Estudiante;
-import util.ResponsabilidadesTrabajador;
-import util.TiposResponsabilidad;
+import models.usuarios.Persona;
+import models.usuarios.Usuario;
 
 public class SeguridadInformatica implements VerificadorEstudiante, VerificadorEmpleado {
 
-	public boolean tieneCuentaUsuarioAbierta(ResponsabilidadesEmpleados r) {
-		return (r.getResponsabilidades().contains(ResponsabilidadesTrabajador.CUENTA_USUARIO));
-	}
+	private HashMap<Persona,Usuario> usuariosCujae;
 	
-	public boolean tieneCuentaUsuarioAbierta(ResponsabilidadesEstudiantes r) {
-		return (r.getResponsabilidades().contains(TiposResponsabilidad.CUENTA_USUARIO));
+	public boolean tieneCuentaUsuarioAbierta(Persona p) {
+		return usuariosCujae.containsKey(p);
 	}
 	
 	@Override
-	public boolean verificarRequisitos(ResponsabilidadesEmpleados responsabilidadesTrabajador) {
-		return tieneCuentaUsuarioAbierta(responsabilidadesTrabajador);
+	public boolean verificarRequisitos(Estudiante e) {
+		return tieneCuentaUsuarioAbierta(e);
 	}
 
 	@Override
-	public boolean verificarRequisitos(ResponsabilidadesEstudiantes responsabilidadesEstudiantes) {
-		return tieneCuentaUsuarioAbierta(responsabilidadesEstudiantes);
+	public boolean verificarRequisitos(Empleado e) {
+		return tieneCuentaUsuarioAbierta(e);
 	}
 	
-	public void cerrarCuenta(Estudiante e, ArrayList<ResponsabilidadesEstudiantes> responsabilidades) {
-		boolean encontrado = false;
-        for (int i = 0; i < responsabilidades.size() && !encontrado; i++) {
-
-            encontrado = responsabilidades.get(i).getEstudiante().equals(e);
-            if (encontrado) {
-                responsabilidades.get(i).getResponsabilidades().remove(TiposResponsabilidad.CUENTA_USUARIO);
-            }
-
-        }
-	}
-	
-	public void cerrarCuenta(Empleado e, ArrayList<ResponsabilidadesEmpleados> responsabilidades) {
-		boolean encontrado = false;
-        for (int i = 0; i < responsabilidades.size() && !encontrado; i++) {
-
-            encontrado = responsabilidades.get(i).getEmpleado().equals(e);
-            if (encontrado) {
-                responsabilidades.get(i).getResponsabilidades().remove(ResponsabilidadesTrabajador.LIBROS_BIBLIOTECA);
-            }
-
-        }
+	public void cerrarCuenta(Persona p) {
+		usuariosCujae.remove(p);
 	}
 	
 	 @Override
-	 public ArrayList<Empleado> getEmpleadosPendientes(ArrayList<ResponsabilidadesEmpleados> responsabilidades) {
-	        ArrayList<Empleado> es = new ArrayList<>();
-	        for (ResponsabilidadesEmpleados r : responsabilidades) {
-	            if (verificarRequisitos(r)) {
-	                es.add(r.getEmpleado());
-	            }
+	    public ArrayList<Estudiante> getEstudiantesPendientes() {
+	    	
+	    	ArrayList<Estudiante> estudiantes = new ArrayList<>();
+	        
+	    	Set<Persona> estudiantesPendientes = usuariosCujae.keySet();
+	        
+	        for (Persona p: estudiantesPendientes) {
+	        	if (p instanceof Estudiante) estudiantes.add(((Estudiante)p));
 	        }
-	        return es;
+	   	
+	        return estudiantes;
 	    }
-
-	@Override
-	public ArrayList<Estudiante> getEstudiantesPendientes(ArrayList<ResponsabilidadesEstudiantes> responsabilidades) {
-	    ArrayList<Estudiante> es = new ArrayList<>();
-        for (ResponsabilidadesEstudiantes r : responsabilidades) {
-            if (verificarRequisitos(r)) {
-                es.add(r.getEstudiante());
-            }
-        }
-        return es;
+	    
+	    @Override
+	    public ArrayList<Empleado> getEmpleadosPendientes() {
+	    	
+	    	ArrayList<Empleado> nombresEmpleados = new ArrayList<>();
+	        
+	    	Set<Persona> empleadosPendientes = usuariosCujae.keySet();
+	        
+	        for (Persona p: empleadosPendientes) {
+	        	if(p instanceof Empleado) nombresEmpleados.add(((Empleado)p));
+	        }
+	   	
+	        return nombresEmpleados;
+	    
 	}
 
 }

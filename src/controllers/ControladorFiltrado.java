2d852fa271f801841d6dee25797d0d6d0e7fd33d
eduarfonseca <fonsecaeduar136@gmail.com>
2023-12-02 12:35:38 -0500

    g

--- src/controllers/ControladorFiltrado.java
+++ src/controllers/ControladorFiltrado.java
@@ -17,7 +17,59 @@ public class ControladorFiltrado {
 
     public static ArrayList<SolicitudBajaEstudiante> obtenerSolicitudesBajaEstudiantesFiltradas(
             HashMap<String, String> map) {
-        return Secretaria.gestorEstudiantes().getGestorSolicitudes()
-                .filtradoDinamicoSolicitudBajaEstudiantes(map);
+        return Secretaria.gestorEstudiantes().getGestorSolicitudes().filtradoDinamicoSolicitudBajaEstudiantes(map);
     }
+}
+public ArrayList<Solicitud> filtradoDinamicoSolicitudEstudiantes(Map<String, String> filtros) {
+    HashMap<Estudiante, Set<Solicitud>> solicitudesEstudianes = Secretaria.getGestorSolicitudes().getSolicitudes();
+    ArrayList<Solicitud> solicitudes = new ArrayList<>();
+
+    for (Map.Entry<Estudiante, Set<Solicitud>> solicitudesEstudianes : solicitudesEstudianes.entrySet()) {
+        Estudiante estudiante = solicitudesEstudianes.getKey();
+        Set<Solicitud> solicitudes = solicitudesEstudianes.getValue();
+        for (Map.Entry<String, String> filtro : filtros.entrySet()) {
+            String atributo = filtro.getKey();
+            String valor = filtro.getValue();
+
+            switch (atributo) {
+                case "anio":
+                    if (!(solictud.getAnioExpedicion() == Integer.parseInt(valor))) {
+                        cumpleFiltros = false;
+                    }
+                    break;
+                case "estado":
+                    if (!solictud.getEstado().toString().equals(valor)) {
+                        cumpleFiltros = false;
+                    }
+                    break;
+                case "motivo":
+                    if (!solictud.getMotivo().toString().equals(valor)) {
+                        cumpleFiltros = false;
+                    }
+                    break;
+                case "buscar":
+                    if ((buscarBajaPorNombreOCi(solictud, valor)) == null) {
+                        cumpleFiltros = false;
+                    }
+                    break;
+            }
+        }
+
+        if (cumpleFiltros) {
+            solicitudesBajaFiltradas.add(solictud);
+        }
+    }
+
+    return solicitudes;
+}
+
+public SolicitudBajaEstudiante buscarBajaPorNombreOCi(SolicitudBajaEstudiante solicitud, String valor) {
+    boolean result = true;
+
+    if (!(solicitud.getEstudiante().getNombreCompleto().toLowerCase().contains(valor.toLowerCase())
+            || solicitud.getEstudiante().getCi().toLowerCase().contains(valor.toLowerCase()))) {
+        result = false;
+    }
+
+    return result ? solicitud : null;
 }

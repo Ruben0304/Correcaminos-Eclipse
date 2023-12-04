875459f5ccf93d5bf4507ed26bd4b9d340f123ba
Ruben Hernandez <hernandzruben9@gmail.com>
2023-12-04 12:59:08 -0500

    b

--- src/data/ObtenerEstudiantesConLibrosDocentesPendientes.java
+++ src/data/ObtenerEstudiantesConLibrosDocentesPendientes.java
@@ -26,6 +26,9 @@ public class ObtenerEstudiantesConLibrosDocentesPendientes {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        if (true) {
+            
+        }
         return estudiantes;
 	}
 

5267b26605b5936fe49cba8150a5e2a4b2ce8e1f
eduarfonseca <fonsecaeduar136@gmail.com>
2023-12-08 18:25:00 -0500

    fxfgv

--- src/data/ObtenerEstudiantes.java
+++ src/data/ObtenerEstudiantes.java
@@ -5,17 +5,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.reflect.Type;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import models.usuarios.Becado;
-import models.usuarios.Empleado;
 import models.usuarios.Estudiante;
 
 public class ObtenerEstudiantes {
